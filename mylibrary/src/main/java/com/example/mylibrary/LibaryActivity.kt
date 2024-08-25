c41b6253a66f385116d98c9a44ade306ed210547
AzureAD\ChiragGrover <chirag.grover@firminiq.com>
2024-08-25 12:53:28 +0530

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/LibaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylibrary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LibaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
